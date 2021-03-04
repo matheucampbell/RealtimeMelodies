a1c646adb6c015b2ae0f9dbb9950952f17892c94
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-03-04 12:41:57 -0500

    Update mag_start.py

--- mag_start.py
+++ mag_start.py
@@ -5,15 +5,16 @@ import tensorflow
 print("Starting...")
 
 # Creating Sequence
-mel = note_seq.protobuf.music_pb2.NoteSequence()
+mel = note_seq.protobuf.music_pb2.NoteSequence()  # Initialize NoteSequence object
 note_list = ((61, 0, 1), (61, 1, 1.5), (64, 1.5, 2), (66, 2, 2.5), (69, 2.5, 3),
              (68, 3, 4), (64, 4, 4.5), (66, 4.5, 5), (64, 5, 5.5), (63, 5.5, 6),
-             (61, 6, 7), (60, 7, 8))
+             (61, 6, 7), (60, 7, 8))  # List of notes in the form (freq, start, end)
 
-for note in  note_list:
+for note in  note_list:  # Add all the notes
     mel.notes.add(pitch=note[0], start_time=note[1], end_time=note[2],
                   velocity=80)
 
+#  Convert note_seq to MIDI for storage and playback
 note_seq.sequence_proto_to_midi_file(mel, 'Input/in.mid')
 
 # Import Dependencies
@@ -23,14 +24,14 @@ from note_seq.protobuf import generator_pb2
 from note_seq.protobuf import music_pb2
 
 # Initialize Model
-bundle = sequence_generator_bundle.read_bundle_file('Src/basic_rnn.mag')
+bundle = sequence_generator_bundle.read_bundle_file('Src/basic_rnn.mag')  # Loads model for use
 generator_map = melody_rnn_sequence_generator.get_generator_map()
 melody_rnn = generator_map['basic_rnn'](checkpoint=None, bundle=bundle)
 melody_rnn.initialize()
 
 # Model Parameters
 steps = 16
-tmp = 1.0
+tmp = 1.0  # Measure of the generation's "temperature". Higher = More scattered
 
 # Initialize Generator
 gen_options = generator_pb2.GeneratorOptions()
