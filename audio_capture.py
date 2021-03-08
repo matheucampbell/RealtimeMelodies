9b10c720668c293d1209965791f0d0e44b6e40cb
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-03-08 12:33:50 -0500

    Update audio_capture.py

--- audio_capture.py
+++ audio_capture.py
@@ -1,6 +1,22 @@
-import sounddevice as sd
+from matplotlib import pyplot as plt
+import numpy as np
+import PyAudio
 import scipy
 
-while True:
-  sd.rec
-  
+CHUNKSIZE = 1024
+arr_data = np.zeroes()
+
+p = pyaudio.PyAudio()
+
+with p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
+            frames_per_buffer=CHUNKSIZE) as stream:
+  while True:
+    arr_data += np.frombuffer(stream.read(CHUNKSIZE), dtype=np.int16)
+    plt.plot(arr_data)
+    plt.show()
+    
+    fft = numpy.fft.fft(arr_data)
+    plt.plot(fft)
+    
+
+p.terminate()
