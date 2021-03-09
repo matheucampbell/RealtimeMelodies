18835bdb88ea4356f9386cc4c509836784e57af8
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-03-09 17:28:29 -0500

    Update capture_FFT.py

--- capture_FFT.py
+++ capture_FFT.py
@@ -4,10 +4,11 @@ import pyaudio  # Audio interface
 from scipy.fft import rfft, rfftfreq  # Scientific functions
 
 cycle_num = 0  # Measures number of measurements of size CHUNKSIZE
+data = False
+
 CHUNKSIZE = 1024  # Frames to capture
 SAMPLING_RATE = 44100  # Standard 44.1 kHz sampling rate
-CYCLES = 43 # Number of cycles
-data = False
+CYCLES = 250 # Number of cycles
 
 p = pyaudio.PyAudio()  # Initialize PyAudio object
 
@@ -28,19 +29,25 @@ while cycle_num < CYCLES:  # Capture CHUNKSIZE audio for CYCLES cycles
         data = True
 
 # Plots the measured frequency and saves the result
-plt.plot(freq_data[:1024])
+plt.plot(freq_data[:CHUNKSIZE])
 plt.title("Raw Microphone Input")
 plt.savefig("Output/Waveform.png")
 
 yf = rfft(freq_data)  # Calculates FFT for freq_data
-n = CHUNKSIZE * CYCLES
 # Returns an array of length n of bins in cycles per second
-xf = rfftfreq(n, 1/SAMPLING_RATE)
+xf = rfftfreq(freq_data.size, 1/SAMPLING_RATE)
 
-# Plot absolute value of FFT against the generated frequency bins
+# Plot absolute value of FFT with the generated frequency bins
 plt.plot(xf, np.abs(yf))
 plt.title("Fast Fourier Transform")
 plt.savefig("Output/FastFourierTransform.png")
+
+# Calculate peak frequency
+peak = np.where(np.abs(yf) == np.abs(yf).max())[0][0]
+peak = peak / ((CHUNKSIZE*CYCLES)/SAMPLING_RATE)
+
+print(f"Peak frequency from {str(CHUNKSIZE*CYCLES/SAMPLING_RATE)} seconds of " +
+      f"audio was {str(peak)} Hz.")
 
 # Cleanup
 stream.stop_stream()
