b77f07d3c834548dd710cdcb1a73663087e14215
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-03-09 12:18:37 -0500

    Update audio_capture.py

--- audio_capture.py
+++ audio_capture.py
@@ -1,29 +1,30 @@
-from matplotlib import pyplot as plt
-import numpy as np
-import pyaudio
-import scipy
+from matplotlib import pyplot as plt  # Data visualization module
+import numpy as np  # Array operations/indexing
+import pyaudio  # Audio interface
 
-cycle_num = 0
-CHUNKSIZE = 1024
-CYCLES = 2
-freq_data = None
+cycle_num = 0  # Measures number of measurements of size CHUNKSIZE
+CHUNKSIZE = 1024  # Frames to capture
+CYCLES = 9   # Number of cycles
+freq_data = None  # Variable to store microphone input
 
-p = pyaudio.PyAudio()
+p = pyaudio.PyAudio()  # Initialize PyAudio object
 
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
-                frames_per_buffer=CHUNKSIZE)
+                frames_per_buffer=CHUNKSIZE)  # Open stream with standard parameters
 
-while cycle_num <= CYCLES:
+while cycle_num < CYCLES:
     cycle_num += 1
     print(f"Cycle {cycle_num}")
 
-    new = np.frombuffer(stream.read(CHUNKSIZE), np.int16)
-    freq_data = np.hstack((freq_data, new))
+    new = np.frombuffer(stream.read(CHUNKSIZE), np.int16)  # Reads stream and converts from bytes to amplitudes
+    freq_data = np.hstack((freq_data, new))  # Combines newest frame with all previous frames
 
+# Plots the measured frequency and saves the result
 plt.plot(freq_data)
 plt.title("Raw Microphone Input")
 plt.savefig("Output/Waveform.png")
 
+# Cleanup
 stream.stop_stream()
 stream.close()
 p.terminate()
