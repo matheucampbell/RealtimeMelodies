69d3ca2b79faf65b5cd895a37cacb6fe02a41077
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-03-09 16:25:09 -0500

    Update capture_FFT.py

--- capture_FFT.py
+++ capture_FFT.py
@@ -1,42 +1,46 @@
 from matplotlib import pyplot as plt  # Data visualization
 import numpy as np  # Array operations/indexing
 import pyaudio  # Audio interface
-from sccipy.fft import fft, fftfreq  # Scientific functions
+from scipy.fft import rfft, rfftfreq  # Scientific functions
 
 cycle_num = 0  # Measures number of measurements of size CHUNKSIZE
 CHUNKSIZE = 1024  # Frames to capture
-SAMPLING_RATE = 44100  # Standard 44.1 kHz sampling
-CYCLES = 9   # Number of cycles
-freq_data = None  # Variable to store microphone input
+SAMPLING_RATE = 44100  # Standard 44.1 kHz sampling rate
+CYCLES = 43 # Number of cycles
+data = False
 
 p = pyaudio.PyAudio()  # Initialize PyAudio object
 
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=SAMPLINGRATE, input=True,
-                frames_per_buffer=CHUNKSIZE)  # Open stream with standard parameters
+# Open stream with standard parameters
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=SAMPLING_RATE,
+                input=True, frames_per_buffer=CHUNKSIZE)
 
-while cycle_num < CYCLES:  # Capture audio in chunks of size CHUNKSIZE for CYCLES cycles
+while cycle_num < CYCLES:  # Capture CHUNKSIZE audio for CYCLES cycles
     cycle_num += 1
-    print(f"Cycle {cycle_num}")
 
-    new = np.frombuffer(stream.read(CHUNKSIZE), np.int16)  # Reads stream and converts from bytes to amplitudes
-    freq_data = np.hstack((freq_data, new))  # Combines newest frame with all previous frames
+    # Reads stream and converts from bytes to amplitudes
+    new = np.frombuffer(stream.read(CHUNKSIZE), np.int16)
+
+    if data:
+        freq_data = np.hstack((freq_data, new))  # Adds new chunk
+    else:
+        freq_data = new
+        data = True
 
 # Plots the measured frequency and saves the result
-plt.plot(freq_data)
+plt.plot(freq_data[:1024])
 plt.title("Raw Microphone Input")
 plt.savefig("Output/Waveform.png")
 
-yf = fft(freq_data)  # Calculate FFT of freq_data
+yf = rfft(freq_data)  # Calculates FFT for freq_data
 n = CHUNKSIZE * CYCLES
-xf = fftfreq(n, 1/SAMPLINGRATE)  # Returns an array of length n of bins in cycles per second
+# Returns an array of length n of bins in cycles per second
+xf = rfftfreq(n, 1/SAMPLING_RATE)
 
-plt.plot(xf, np.abs(yf))  # Plot absolute value of fft against the generated frequency bins
+# Plot absolute value of FFT against the generated frequency bins
+plt.plot(xf, np.abs(yf))
 plt.title("Fast Fourier Transform")
 plt.savefig("Output/FastFourierTransform.png")
-
-# Find peak frequency
-peak = np.maximum(yf)
-print(f"The peak frequency of this audio sample is {str(peak)} Hz.")
 
 # Cleanup
 stream.stop_stream()
