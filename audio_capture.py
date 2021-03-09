b1af04fa33926a998be82abb2168028883948ba3
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-03-09 13:06:46 -0500

    Update audio_capture.py

--- audio_capture.py
+++ audio_capture.py
@@ -4,13 +4,14 @@ import pyaudio  # Audio interface
 from sccipy.fft import fft, fftfreq  # Scientific functions
 
 cycle_num = 0  # Measures number of measurements of size CHUNKSIZE
-CHUNKSIZE = 1024  # Frames to capture, 
+CHUNKSIZE = 1024  # Frames to capture
+SAMPLING_RATE = 44100  # Standard 44.1 kHz sampling
 CYCLES = 9   # Number of cycles
 freq_data = None  # Variable to store microphone input
 
 p = pyaudio.PyAudio()  # Initialize PyAudio object
 
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=SAMPLINGRATE, input=True,
                 frames_per_buffer=CHUNKSIZE)  # Open stream with standard parameters
 
 while cycle_num < CYCLES:
@@ -25,14 +26,17 @@ plt.plot(freq_data)
 plt.title("Raw Microphone Input")
 plt.savefig("Output/Waveform.png")
 
+yf = fft(freq_data)  # Calculate FFT of freq_data
 n = CHUNKSIZE * CYCLES
+xf = fftfreq(n, 1/SAMPLINGRATE)  # Returns an array of length n of bins in cycles per second
 
-yf = fft(freq_data)
-xf = fftfreq(n, 1/44100)
-
-plt.plot(xf, np.abs(yf))
+plt.plot(xf, np.abs(yf))  # Plot absolute value of fft against the generated frequency bins
 plt.title("Fast Fourier Transform")
 plt.savefig("Output/FastFourierTransform.png")
+
+# Find peak frequency
+peak = np.maximum(yf)
+print(f"The peak frequency of this audio sample is {str(peak)} Hz.")
 
 # Cleanup
 stream.stop_stream()
