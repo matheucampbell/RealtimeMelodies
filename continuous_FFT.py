717c62a3a78438c328a8a77b241b16154b3295a4
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-04-13 12:47:20 -0400

    Update continuous_FFT.py

--- continuous_FFT.py
+++ continuous_FFT.py
@@ -34,28 +34,34 @@ class Note:  # Note object to store input for note_seq
         self.finished = True
 
         return self
-
+    
     def add_rests(self, rest_list, main_seq):
         new_notes = []
         rests = [rest for rest in rest_list if
                  self.start <= rest[0] <= self.end or
                  self.start <= rest[1] <= self.end]
 
+        if not rests:
+            return main_seq
+
         for x in range(len(rests)):
             new_notes.append((rests[x-1][1], rests[x][0]))
-            new_notes.remove(new_notes[0])
+            
+        new_notes.remove(new_notes[0])
 
-            if rests[0][0] > self.start:
-                new_notes.append((self.start, rests[0][0]))
-            if rests[-1][1] < self.end:
-                new_notes.append((rests[-1][1], self.end))
+        if rests[0][0] > self.start:
+            new_notes.append((self.start, rests[0][0]))
+        if rests[-1][1] < self.end:
+            new_notes.append((rests[-1][1], self.end))
 
+        new_notes.sort()
+        
         for note in new_notes:
             new = Note(self.midi, note[0], note[1], True, False)
             main_seq.append(new)
 
         main_seq.remove(self)
-
+        
         return main_seq
 
 # Calculates peak frequency of one chunk of audio
