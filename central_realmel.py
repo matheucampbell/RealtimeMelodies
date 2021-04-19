9bd66fc42a9dac91ee48323dcb92a8411ae7e90c
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-04-19 18:29:37 -0400

    Update central_realmel.py

--- central_realmel.py
+++ central_realmel.py
@@ -1,11 +1,8 @@
 import melody_extension
 import sys
 
-CHUNK_DURATION = round(float(sys.argv[1]), 3)  # Defines chunk duration in sec
-MIN_VOLUME = round(float(sys.argv[2]))  # Minimum max volume to qualify as note
-MIN_REST = float(sys.argv[3])  # Minimum length of a rest in seconds
-MEL_MIN = round(float(sys.argv[4]))
-REST_MAX = round(float(sys.argv[5]))
+FINAL_REST, MIN_VOLUME = melody_extension.calibrate()
 
-melody_extension.listen_and_extend(CHUNK_DURATION, MIN_VOLUME, MIN_REST,
-                                   MEL_MIN, REST_MAX)
+print(FINAL_REST, FINAL_REST)
+
+melody_extension.listen_and_extend(.15, MIN_VOLUME, .1, FINAL_REST)
