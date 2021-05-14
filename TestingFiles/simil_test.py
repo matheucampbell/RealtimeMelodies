ee2c3e43bfc1f55ecf41fa2b9a811a4df920efa0
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-05-14 12:42:02 -0400

    Update simil_test.py

--- TestingFiles/simil_test.py
+++ TestingFiles/simil_test.py
@@ -1,19 +1,14 @@
 import sympy as sym
 
-
-sym.init_printing(use_unicode=True)
-
 x, y, z = sym.symbols('x y z')
-vals = []
 
 trg = ((65, 0, 2.3), (63, 2.3, 3.1), (60, 3.1, 3.9))
 terp = ((64, 0, 1.7), (65, 1.7, 2.3), (63, 2.3, 2.8), (60, 3.1, 3.9))
 
 funcs = ((note[0], sym.And(note[1]<=x, x<note[2])) for note in trg)
 
-while (new_val, new_cond) = next(funcs)
-    trg_pce = sym.Piecewise(next(funcs), (y, z))
-    trg_pce = trg_pce.subs([(y, new_val), (z, new_cond)])
+(val, cond) = next(funcs)
+trg_pce = sym.Piecewise((val, cond), (y, z))
 
-print(trg_pce)
-                       
+while (interval := next(funcs, None)):
+    trg_pce = trg_pce.subs([(y, interval[0]), (z, interval[1])])
