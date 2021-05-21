538e9265c4c2765505e37674a63c7b2de3fc7fe6
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-05-21 12:49:55 -0400

    Update simil_test.py

--- TestingFiles/simil_test.py
+++ TestingFiles/simil_test.py
@@ -1,11 +1,13 @@
 import sympy as sym
 import numpy as np
 
+from sympy import solveset, S
+
 x, y, z = sym.symbols('x y z')
 f, g = sym.symbols('target interpreted', cls=sym.Function)
 
-trg = ((65, 0, 2.3), (63, 2.3, 3.1), (60, 3.1, 3.9))
-terp = ((64, 0, 1.7), (65, 1.7, 2.3), (63, 2.3, 2.8), (60, 3.2, 3.9))
+trg = ((65, 0, 1.2), (63, 2.3, 3.1), (60, 3.1, 3.9))
+terp = ((64, 0, 1.7), (65, 2.0, 2.3), (63, 2.3, 2.8), (60, 3.2, 3.9))
 
 trg_funcs = [(note[0], sym.And(note[1] <= x, x <= note[2])) for note in trg]
 terp_funcs = [(note[0], sym.And(note[1] <= x, x <= note[2])) for note in terp]
@@ -16,6 +18,8 @@ g = sym.Piecewise(*terp_funcs)
 
 def check_cont(f1, f2, y):  # XOR for two given functions for a given x
     if bool(f1.subs(x, y) == sym.nan) != bool(f2.subs(x, y) == sym.nan):
+        return y
+    elif f1.subs(x, y) == sym.nan and f2.subs(x, y) == sym.nan:
         return y
     else:
         return None
