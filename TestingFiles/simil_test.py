1ed4a341eb1ef93a1c46b44b50dd1bf939802744
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2021-05-24 12:51:30 -0400

    Update simil_test.py

--- TestingFiles/simil_test.py
+++ TestingFiles/simil_test.py
@@ -6,8 +6,8 @@ from sympy import solveset, S
 x, y, z = sym.symbols('x y z')
 f, g = sym.symbols('target interpreted', cls=sym.Function)
 
-trg = ((65, 0, 1.2), (63, 2.3, 3.1), (60, 3.1, 3.9))
-terp = ((64, 0, 1.7), (65, 2.0, 2.3), (63, 2.3, 2.8), (60, 3.2, 3.9))
+trg = ((65, 0, 1.2), (63, 1.2, 3.1), (60, 3.1, 3.9))
+terp = ((64, 0, 1.7), (65, 1.7, 2.3), (63, 2.3, 2.8), (60, 2.8, 3.9))
 
 trg_funcs = [(note[0], sym.And(note[1] <= x, x <= note[2])) for note in trg]
 terp_funcs = [(note[0], sym.And(note[1] <= x, x <= note[2])) for note in terp]
@@ -53,6 +53,15 @@ def convert_to_intervals(points):
     return ret
     
 vals = find_disconts(f, g, trg[-1][2])
-print(convert_to_intervals(vals))
+int_vals = convert_to_intervals(vals)
 
+total = 0
+for spc in int_vals:
+    total += sym.integrate(np.abs(f - g), (x, spc[0], spc[1]))
+
+domain_total = sum([spc[1] - spc[0] for spc in int_vals])
+avg = total / domain_total
+
+print(avg)
 res = sym.integrate(np.abs(f - g), (x, 0, 3.9)) / 3.9
+print(res)
